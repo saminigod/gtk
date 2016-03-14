e2ceea828957f03dc97ba31b012376d003075467
Benjamin Otte <otte@redhat.com>
2016-03-14 20:27:55 +0100

    css: Add a consumer object to consume_token()
    This is to allow backreferences from code analysis frameworks.

--- gtk/gtkcsstokensource.c
+++ gtk/gtkcsstokensource.c
@@ -40,7 +40,8 @@ gtk_css_token_source_tokenizer_finalize (GtkCssTokenSource *source)
 }
 
 static void
-gtk_css_token_source_tokenizer_consume_token (GtkCssTokenSource *source)
+gtk_css_token_source_tokenizer_consume_token (GtkCssTokenSource *source,
+                                              GObject           *consumer)
 {
   GtkCssTokenSourceTokenizer *tok = (GtkCssTokenSourceTokenizer *) source;
 
@@ -54,9 +55,7 @@ gtk_css_token_source_tokenizer_get_token (GtkCssTokenSource   *source)
 
   if (gtk_css_token_is (&tok->current_token, GTK_CSS_TOKEN_EOF))
     {
-      do {
-       gtk_css_tokenizer_read_token (tok->tokenizer, &tok->current_token);
-      } while (gtk_css_token_is (&tok->current_token, GTK_CSS_TOKEN_COMMENT));
+      gtk_css_tokenizer_read_token (tok->tokenizer, &tok->current_token);
 
 #if 0
       if (!gtk_css_token_is (&tok->current_token, GTK_CSS_TOKEN_EOF)) {
@@ -116,7 +115,8 @@ gtk_css_token_source_part_finalize (GtkCssTokenSource *source)
 }
 
 static void
-gtk_css_token_source_part_consume_token (GtkCssTokenSource *source)
+gtk_css_token_source_part_consume_token (GtkCssTokenSource *source,
+                                         GObject           *consumer)
 {
   GtkCssTokenSourcePart *part = (GtkCssTokenSourcePart *) source;
   const GtkCssToken *token;
@@ -128,7 +128,7 @@ gtk_css_token_source_part_consume_token (GtkCssTokenSource *source)
         return;
     }
 
-  gtk_css_token_source_consume_token (part->source);
+  gtk_css_token_source_consume_token_as (part->source, consumer);
 }
 
 static const GtkCssToken *
@@ -174,6 +174,8 @@ gtk_css_token_source_new_for_part (GtkCssTokenSource *source,
   part = gtk_css_token_source_new (GtkCssTokenSourcePart, &GTK_CSS_TOKEN_SOURCE_PART);
   part->source = gtk_css_token_source_ref (source);
   part->end_type = end_type;
+  gtk_css_token_source_set_consumer (&part->parent,
+                                     gtk_css_token_source_get_consumer (source));
 
   return &part->parent;
 }
@@ -208,6 +210,8 @@ gtk_css_token_source_unref (GtkCssTokenSource *source)
 
   source->klass->finalize (source);
 
+  g_clear_object (&source->consumer);
+
   g_free (source);
 }
 
@@ -231,7 +235,7 @@ gtk_css_token_source_consume_token_as (GtkCssTokenSource *source,
         source->blocks = g_slist_remove (source->blocks, source->blocks->data);
     }
 
-  source->klass->consume_token (source);
+  source->klass->consume_token (source, consumer);
 
   token = gtk_css_token_source_get_token (source);
   switch (token->type)
@@ -262,7 +266,17 @@ gtk_css_token_source_consume_token_as (GtkCssTokenSource *source,
 const GtkCssToken *
 gtk_css_token_source_get_token (GtkCssTokenSource *source)
 {
-  return source->klass->get_token (source);
+  const GtkCssToken *token;
+
+  for (token = source->klass->get_token (source);
+       gtk_css_token_is (token, GTK_CSS_TOKEN_COMMENT);
+       token = source->klass->get_token (source))
+    {
+      /* use vfunc here to avoid infloop */
+      source->klass->consume_token (source, source->consumer);
+    }
+
+  return token;
 }
 
 void
@@ -379,5 +393,18 @@ gtk_css_token_source_deprecated (GtkCssTokenSource *source,
   gtk_css_token_source_emit_error (source, error);
   g_error_free (error);
   va_end (args);
+}
+
+GObject *
+gtk_css_token_source_get_consumer (GtkCssTokenSource *source)
+{
+  return source->consumer;
+}
+
+void
+gtk_css_token_source_set_consumer (GtkCssTokenSource *source,
+                                   GObject           *consumer)
+{
+  g_set_object (&source->consumer, consumer);
 }
 
